c9b7e0c7e682405d550c7afd5ba5fc266e6efebc
petrkvapil91 <petrkvapil91@gmail.com>
2020-12-08 18:53:50 +0100

    to do list upgrade

--- To-Do List/app.js
+++ To-Do List/app.js
@@ -3,17 +3,40 @@ const addButton = document.getElementById('addItem');
 const input = document.querySelector('#itemInput');
 const itemList = document.getElementById('list');
 
+
 addButton.addEventListener('click', () => {
-    const item = input.value;
     const newItem = document.createElement('li');
-    newItem.append(item);   
-    itemList.appendChild(newItem);
-    input.value = "";
+    const item = input.value;
+        newItem.append(item);
+        itemList.appendChild(newItem);
+        input.value = "";
+    
+    const deleteBtn = document.createElement('button');
+        newItem.append(deleteBtn);
+
+        deleteBtn.innerText = 'X'
+        deleteBtn.style.border = 'none'
+
+    deleteBtn.addEventListener('click', () => {
+        newItem.remove();
+    })
+
+    const doneBtn = document.createElement('button');
+        newItem.append(doneBtn);
+
+        doneBtn.innerText = 'Done'
+        doneBtn.style.border = 'none'
+
+    doneBtn.addEventListener('click', () => {
+        newItem.style.textDecoration = "line-through"
+    })
 })
 
 const clearButton = document.getElementById('deleteBtn');
 
-    clearButton.addEventListener('click', () => {
-            itemList.removeChild();
-    })
+clearButton.addEventListener('click', () => {
+    while (itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild);
+    }
+})
 
